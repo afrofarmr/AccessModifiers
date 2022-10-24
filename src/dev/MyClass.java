895262c92006fc3714522b320aad4cc1d8bc02e0
afrofarmr <oluwafewa.adegoke@gmail.com>
2022-10-24 11:53:43 +0800

    Initial commit

---
+++ src/dev/MyClass.java
@@ -0,0 +1,10 @@
+package dev;
+
+import qa.QaClass;
+
+public class MyClass {
+    public static void main(String[] args){
+        QaClass qaClass = new QaClass();
+        qaClass.myMethod();
+    }
+}
---
+++ src/qa/QaClass.java
@@ -0,0 +1,9 @@
+package qa;
+
+public class QaClass {
+    String str = "My String";
+
+    public void myMethod(){
+        System.out.println("qa class my method");
+    }
+}
